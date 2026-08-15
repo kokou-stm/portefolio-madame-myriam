67af08eef863a82a2372f65d93b5ffca0d8cb714
sekpona kokou <sekponakokou@sekponas-macbook-pro.tail7ee796.ts.net>
2026-08-16 01:52:10 +0200

    Implement lazy poster thumbnail play button pattern to guarantee 100% video loading reliability

--- cv/models.py
+++ cv/models.py
@@ -567,9 +567,9 @@ class Video(models.Model):
 
     @property
     def embed_url(self):
-        return f"https://www.youtube-nocookie.com/embed/{self.youtube_id}?rel=0"
+        return f"https://www.youtube-nocookie.com/embed/{self.youtube_id}?rel=0&autoplay=1"
 
     @property
     def thumbnail_url(self):
-        return f"https://img.youtube.com/vi/{self.youtube_id}/hqdefault.jpg"
+        return f"https://i.ytimg.com/vi/{self.youtube_id}/hqdefault.jpg"
 
